e6cce83f687627680192cadd4b9f2bd77c8ec924
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-11 23:43:20 -0300

    Refactor Convidados class and add user input for guest information

--- src/Array/EXArrayList03.java
+++ src/Array/EXArrayList03.java
@@ -3,6 +3,7 @@ package Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /*
 A ideia desse sistema é criar uma lista de presentes de casamento, assim é necessário que o sistema verifique quem é a pessoa e o seu presente;*/
@@ -10,16 +11,16 @@ A ideia desse sistema é criar uma lista de presentes de casamento, assim é nec
 class Convidados{
     String nomeConvidado;
     String presenteConvidado;
-    boolean presencaConvidado;
+    String presencaConvidado;
 
-    Convidados( String Convidado, String presente, boolean presenca){
+    Convidados( String Convidado, String presente, String presenca){
         this.nomeConvidado = Convidado;
         this.presenteConvidado = presente;
         this.presencaConvidado = presenca;
     }
 
     public String exibirLista(){
-        return "Nome do Convidado: " + nomeConvidado +"\nPresente: "+ presenteConvidado + "\nPresença na festa: "+ presencaConvidado;
+        return "\nNome do Convidado: " + nomeConvidado +"\nPresente: "+ presenteConvidado + "\nPresença na festa: "+ presencaConvidado;
     }
     /*
     @Override
@@ -39,13 +40,37 @@ class Convidados{
 
 public class EXArrayList03 {
     public static void main(String[] args) {
-        List<Convidados> convidados_list =  new ArrayList<>();
+        Scanner entrada = new Scanner(System.in);
+        List<Convidados> convidados_list =  new ArrayList<>(Arrays.asList(
+            new Convidados("Irailde Sobral", "Geladeira", "SIM"),
+            new Convidados("Karol Sobral", "Casa no Jardim Aulídia", "SIM"),
+            new Convidados("Eranildo Sobral", "Armário", "SIM"),
+            new Convidados("Filomena Sobral", "Fogão", "SIM"),
+            new Convidados("Reginaldo", "Microondas", "SIM"),
+            new Convidados("Ivanilde Sobral", "Guarda Roupa", "SIM"),
+            new Convidados("Ivanilde Sobral", "Cama Box King", "SIM"),
+            new Convidados("Cláudio e Valdirene", "Máquina de lavar", "SIM")
+        ));
+        System.out.println("Seja bem vindo a Lista de convidados do casamento de Marcos e ébora");
 
-        //convidados.add(new Convidados(null, null, false));
-        convidados_list.add(new Convidados("Irailde", "Geladeira", true));
+        receberDados(entrada, convidados_list);
 
         for (Convidados i : convidados_list) {
             System.out.println(i.exibirLista());
         }
+    }
+
+    public static void receberDados(Scanner entrada, List<Convidados> listaConvidados) {
+        System.out.println("\nInforme seu nome:");
+        String nomeConvidado = entrada.nextLine();
+
+        System.out.println("\nInforme o presente:");
+        String presenteConvidado = entrada.nextLine();
+
+        System.out.println("\nConfirme sua presença para o chá de panela: SIM/NÃO");
+        String presencaConvidado = entrada.nextLine();
+        
+        //convidados.add(new Convidados(null, null, false));
+        listaConvidados.add(new Convidados(nomeConvidado, presenteConvidado, presencaConvidado));
     }
 }
